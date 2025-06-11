b4b232f83618017076583d4ec98c2cd1981db2c0
Shah Nawaz <shuvo1824@hotmail.com>
2025-06-11 22:55:49 +0600

    feat: login handler

--- src/db/queries.test.ts
+++ src/db/queries.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, beforeEach, afterEach } from "bun:test";
 import { Database } from "bun:sqlite";
-import { insertUser } from "./queries";
+import { getUserByEmail, insertUser } from "./queries";
 import { createTestDb } from "../test/test-db";
 
 let db: Database;
@@ -15,7 +15,6 @@ afterEach(() => {
 
 describe("insertUser", () => {
   it("should insert a user into the database", async () => {
-    const db = createTestDb();
     const email = "j6E5t@example.com";
     const password = "password123";
     const userId = await insertUser(db, email, password);
@@ -24,7 +23,6 @@ describe("insertUser", () => {
   });
 
   it("should throw an error if the user is already in the database", async () => {
-    const db = createTestDb();
     const email = "test@example.com";
     const password = "password123";
     await insertUser(db, email, password);
@@ -39,7 +37,6 @@ describe("insertUser", () => {
   });
 
   it("should throw an error if the password is empty", async () => {
-    const db = createTestDb();
     const email = "test@example.com";
     const password = "";
 
@@ -50,5 +47,23 @@ describe("insertUser", () => {
       // @ts-ignore
       expect(error.message).toMatch(/password must not be empty/);
     }
+  });
+});
+
+describe("getUserById", () => {
+  it("returns a user by a given email", async () => {
+    const email = "test@example.com";
+    const password = "password123";
+    await insertUser(db, email, password);
+
+    const user = getUserByEmail(db, email);
+    expect(user).toBeDefined();
+  });
+
+  it("returns null when there is no user by the email", async () => {
+    const email = "test@example.com";
+
+    const user = getUserByEmail(db, email);
+    expect(user).toBeNull();
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,70 @@
 import { Hono } from "hono";
 import { setCookie } from "hono/cookie";
 import { signupValidator } from "./schemas/signup-schema";
-import { insertUser } from "./db/queries";
+import { getUserByEmail, insertUser } from "./db/queries";
 import { dbConn } from "./db/db";
 import { cookieOpts, genenrateToken } from "./helper";
 
 const app = new Hono();
 
-app.post("/api/signup", signupValidator, async (c) => {
-  const db = dbConn();
-  const { email, password } = c.req.valid("json");
+app
+  .post("/api/signup", signupValidator, async (c) => {
+    const db = dbConn();
+    const { email, password } = c.req.valid("json");
 
-  try {
-    const userId = await insertUser(db, email, password);
+    try {
+      const userId = await insertUser(db, email, password);
 
-    const token = await genenrateToken(userId);
+      const token = await genenrateToken(userId);
 
-    setCookie(c, "authToken", token, cookieOpts);
+      setCookie(c, "authToken", token, cookieOpts);
 
-    return c.json({
-      message: "User registered successfully",
-      user: { id: userId, email },
-    });
-  } catch (error) {
-    if (
-      error instanceof Error &&
-      error.message.includes("UNIQUE constraint failed")
-    ) {
-      return c.json({ errors: ["User already exists"] }, 409);
+      return c.json({
+        message: "User registered successfully",
+        user: { id: userId, email },
+      });
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        error.message.includes("UNIQUE constraint failed")
+      ) {
+        return c.json({ errors: ["User already exists"] }, 409);
+      }
+
+      console.log("signup error: ", error);
+      return c.json({ errors: ["Internal server error"] }, 500);
     }
+  })
+  .post("/api/login", signupValidator, async (c) => {
+    const db = dbConn();
+    const { email, password } = c.req.valid("json");
 
-    console.log("signup error: ", error);
-    return c.json({ errors: ["Internal server error"] }, 500);
-  }
-});
+    try {
+      const user = getUserByEmail(db, email);
+      if (!user) {
+        return c.json({ errors: ["Invalid credentials"] }, 401);
+      }
+
+      const passwordMatch = await Bun.password.verify(
+        password,
+        user.password_hash,
+      );
+      if (!passwordMatch) {
+        return c.json({ errors: ["Invalid credentials"] }, 401);
+      }
+
+      const token = await genenrateToken(user.id);
+
+      setCookie(c, "authToken", token, cookieOpts);
+
+      return c.json({
+        message: "Login successful",
+        user: { id: user.id, email: email },
+      });
+    } catch (error) {
+      console.log(error);
+      return c.json({ errors: ["Internal server error"] }, 500);
+    }
+  });
 
 export default app;
