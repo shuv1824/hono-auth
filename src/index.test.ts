0b8ea19dce94fbe068baacf1eb0f46a5d6205da1
Shah Nawaz <shuvo1824@hotmail.com>
2025-06-13 23:21:14 +0600

    test: login endpoint error unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,7 @@ import app from ".";
 import { createTestDb } from "./test/test-db";
 import { Database } from "bun:sqlite";
 import { loginRequest, signupRequest } from "./test/test-helpers";
+import { error } from "console";
 
 let db: Database;
 
@@ -82,5 +83,30 @@ describe("login endpoint", () => {
 
     const cookies = response2.headers.get("set-cookie");
     expect(cookies).toMatch(/authToken=/);
+  });
+
+  it("should return 400 if email and/or password is missing", async () => {
+    const request = loginRequest("", "");
+    const response = await app.fetch(request);
+    const json = await response.json();
+
+    expect(response.status).toBe(400);
+    expect(json).toEqual({
+      errors: ["Invalid email", "Password must be at least 10 characters"],
+    });
+  });
+
+  it("should return 401 if password is incorrect", async () => {
+    const request = signupRequest();
+    await app.fetch(request);
+
+    const request2 = loginRequest("test@test.com", "password456");
+    const response2 = await app.fetch(request2);
+    const json = await response2.json();
+
+    expect(response2.status).toBe(401);
+    expect(json).toEqual({
+      errors: ["Invalid credentials"],
+    });
   });
 });
